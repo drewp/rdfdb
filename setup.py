440109a99273071a58d60b433c61996544b04771
Drew Perttula <drewp@bigasterisk.com>
2018-02-19 01:33:33 -0800

    version 10
    Ignore-this: 2310890d15bde8de1e4a8d58be767034
    
    darcs-hash:b7b3dc1869e32ec82c43ddb6e07a29dd91798748

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='rdfdb',
-    version='10',
+    version='11',
     packages=find_packages(),
     install_requires=[
         'rdflib',
