fdd1856ea58480df6ad39d722766c95419f029c6
Drew Perttula <drewp@bigasterisk.com>
2019-05-24 19:53:14 -0700

    some low-quality mostly generated mypy stubs. should be shared with my other builds
    Ignore-this: 99c013ca3860da8917b3ca52c24308fc
    
    darcs-hash:05f58a532e52cc3ace8c759dc43ffd46a53458be

---
+++ stubs/twisted/python/_oldstyle.pyi
@@ -0,0 +1,3 @@
+from typing import Any
+
+_replaceIf = Any
---
+++ stubs/zope/interface/__init__.pyi
@@ -0,0 +1,5 @@
+from typing import Any
+
+#from zope.interface.declarations import Declaration as Declaration, alsoProvides as alsoProvides, classImplements as classImplements, classImplementsOnly as classImplementsOnly, classProvides as classProvides, directlyProvidedBy as directlyProvidedBy, directlyProvides as directlyProvides, implementedBy as implementedBy, implementer as implementer, implementer_only as implementer_only, implements as implements, implementsOnly as implementsOnly, moduleProvides as moduleProvides, noLongerProvides as noLongerProvides, providedBy as providedBy, provider as provider
+#from zope.interface.interface import Attribute as Attribute, Interface as Interface
+Interface: Any
