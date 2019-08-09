8c71f02f989b2de1a4921640d1ca765e6d9efdb6
drewp <drewp@bigasterisk.com>
2019-08-08 23:15:15 -0700

    release 0.21.0
    Ignore-this: bdd208cf7265ebdfe685b09c89ee8a5a
    
    darcs-hash:2a118b0049045216bbb1be0b26f3b4e545ca4296

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='rdfdb',
-    version='0.20.0',
+    version='0.21.0',
     packages=find_packages(),
     install_requires=[
         'rdflib',
@@ -15,7 +15,7 @@ setup(
         'standardservice', # from --index-url https://projects.bigasterisk.com/
         'scales', # use git+http://github.com/drewp/scales.git@master#egg=scales
         ],
-    url='https://projects.bigasterisk.com/rdfdb/rdfdb-0.20.0.tar.gz',
+    url='https://projects.bigasterisk.com/rdfdb/rdfdb-0.21.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
     entry_points={
